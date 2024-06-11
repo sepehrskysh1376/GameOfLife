7120fbc4c147e655e564bee3eefa6bd6d15948be
sepehrskysh12 <sepehr.sky.sh@gmail.com>
2024-06-11 16:05:17 +0330

    functionalize the World initialization & Printing the World Stat, also adding my first Char.txt for printing purposes

--- GoL.jl
+++ GoL.jl
@@ -1,19 +1,57 @@
+# A 2D world with GoL Rules
+
+    # The function for initializing a world as an Array
+worldIni(nx::Int64, ny::Int64) = zeros(Int64, nx, ny)
+
+# A = worldIni(10, 10) # The world!
 A = [0 0 0 0 0 0 0 0 0
      0 1 0 0 0 0 0 1 0 
      0 0 0 0 0 0 0 0 0
      0 0 0 0 0 0 0 0 0
      0 0 0 0 0 0 0 0 0
-     0 0 0 0 0 0 0 0 0 
+     0 0 1 1 1 0 0 0 0 
+     0 0 0 0 1 0 1 0 0
      0 0 0 0 0 0 0 0 0
-     0 0 0 0 0 0 0 0 0
-     0 0 0 0 0 0 0 0 0]# The world! 
+     1 0 0 0 0 0 0 0 1]# The world! 
 
-for row in 2:size(A)[1]-1
-    for col in 2:size(A)[2]-1
-        print("$(A[row, col]) ")
+    # The function for printing the world status
+function worldPrint(world::Array, shape = "1.txt")
+    """
+    input:
+        world: 
+        | An array which consist of 1 for full cell and 0 for empty cell
+        shape:
+        | An argument which is a file consisting of the shapes using for printing them.
+    output:
+        
+    """
+    aliveCells = floor(Int64, sum(world))
+    deadCells  = floor(Int64, size(world)[1] * size(world)[2] - aliveCells)
+    print("Alive: $aliveCells \t\tDead: $deadCells\n")
+    print("--------------------------------------\n")
+
+    shapeFile = readlines(shape)
+    shapes = Dict(0 => "ERROR", 1 => "ERROR")
+    for i in shapeFile
+        state, char = split(i, " ")
+        if parse(Int64, state) in keys(shapes)
+            shapes[parse(Int64, state)] = char
+        end
     end
-    print("\n")
+
+
+    for row in 1:size(world)[1]
+
+        for col in 1:size(world)[2]
+            print("$(shapes[world[row, col]])")
+        end
+
+        print("\n")
+    end
 end
+
+worldPrint(A)
+
 
 A0 = [1 1 0 
       0 0 0
