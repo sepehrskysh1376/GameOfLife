e345bccba58e54fde911a6bae6c5bcfd9be62f95
sepehrskysh12 <sepehr.sky.sh@gmail.com>
2024-06-12 20:21:30 +0330

    Making rule function & Adding two other showing styles

--- GoL.jl
+++ GoL.jl
@@ -1,21 +1,26 @@
 # A 2D world with GoL Rules
 
+    # Initializations
+K = [1 1 1 
+     1 0 1
+     1 1 1] # The weight distribution of the neighbors
+
     # The function for initializing a world as an Array
 worldIni(nx::Int64, ny::Int64) = zeros(Int64, nx, ny)
 
 # A = worldIni(10, 10) # The world!
 A = [0 0 0 0 0 0 0 0 0
-     0 1 0 0 0 0 0 1 0 
-     0 0 0 0 0 0 0 0 0
-     0 0 0 0 0 0 0 0 0
-     0 0 0 0 0 0 0 0 0
+     0 0 0 0 0 0 0 1 0 
+     0 0 1 1 1 0 0 0 0
+     0 0 1 1 1 0 0 0 0
+     0 0 0 1 0 0 0 0 0
      0 0 1 1 1 0 0 0 0 
-     0 0 0 0 1 0 1 0 0
+     0 0 0 1 0 0 0 0 0
      0 0 0 0 0 0 0 0 0
-     1 0 0 0 0 0 0 0 1]# The world! 
+     0 0 0 0 0 0 0 0 0]# The world! 
 
     # The function for printing the world status
-function worldPrint(world::Array, shape = "1.txt")
+function worldPrint(world::Array, shape = "3.txt")
     """
     input:
         world: 
@@ -52,34 +57,57 @@ end
 
 worldPrint(A)
 
+    # The Update function
+function rule(A::Array, K::Array)
+    """
+    Input:
+        A: is the world Array.
+    Output:
+        K: is the weight distribution of the neighbors
+    """
+    G = zeros(size(A)[1], size(A)[2])
 
-A0 = [1 1 0 
-      0 0 0
-      0 0 1] # starting array
-K = [1 1 1 
-     1 0 1
-     1 1 1] # The (k)orrection array
+    for i in 2:size(A)[1] - 1
+        for j in 2:size(A)[2] - 1
 
-# G = 0 # the function for evolving
-S = sum(A0 .* K) 
-G = 2
-if S < 3 || S > 5
-    G = -1
-elseif S == 3
-    G = 0
-elseif S ∈ [4, 5]
-    G = 1
+            S = sum(A[i-1:i+1, j-1:j+1] .* K)
+
+            Gij = 0 
+            if S < 2 || S > 3
+                Gij = -1
+            elseif S == 2 
+                Gij = 0
+            elseif S ∈ [3]
+                Gij = 1
+            end
+            
+            # Catching Exception about G value
+            if G == 2
+                throw("Something about the G value has gone wrong!!!")
+            end
+            G[i, j] = Gij
+        end
+    end
+    
+    return [floor(Int64, x) for x in G]
 end
 
-# Catching Exception about G value
-if G == 2
-    throw("Something about the G value has gone wrong!!!")
+G = rule(A, K)
+
+ANext = A + G
+
+for i in 1:size(ANext)[1]
+    for j in 1:size(ANext)[2]
+        if ANext[i, j] == -1
+            ANext[i, j] = 0
+        elseif ANext[i, j] == 2
+            ANext[i, j] = 1
+        end
+    end
 end
 
-println(A0 .* K)
-println(G)
 
-
+worldPrint(ANext)
 
 
 
