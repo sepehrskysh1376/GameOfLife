dddbbf79a972192b5c5bde428cb3b40a1a886541
sepehrskysh12 <sepehr.sky.sh@gmail.com>
2024-06-13 22:30:14 +0330

    Adding Update function for evolving the world

--- GoL.jl
+++ GoL.jl
@@ -5,28 +5,32 @@ K = [1 1 1
      1 0 1
      1 1 1] # The weight distribution of the neighbors
 
+A0 = [0 0 0 0 0 0 0 0 0
+      0 0 0 0 0 0 0 1 0 
+      0 0 1 1 1 0 0 0 0
+      0 0 1 1 1 0 0 0 0
+      0 0 0 1 0 0 0 0 0
+      0 0 1 1 1 0 0 0 0 
+      0 0 0 1 0 0 0 0 0
+      0 0 0 0 0 0 0 0 0
+      0 0 0 0 0 0 0 0 0]# The world!
+
+Nt = 10 # The number of time-steps
+
     # The function for initializing a world as an Array
 worldIni(nx::Int64, ny::Int64) = zeros(Int64, nx, ny)
 
 # A = worldIni(10, 10) # The world!
-A = [0 0 0 0 0 0 0 0 0
-     0 0 0 0 0 0 0 1 0 
-     0 0 1 1 1 0 0 0 0
-     0 0 1 1 1 0 0 0 0
-     0 0 0 1 0 0 0 0 0
-     0 0 1 1 1 0 0 0 0 
-     0 0 0 1 0 0 0 0 0
-     0 0 0 0 0 0 0 0 0
-     0 0 0 0 0 0 0 0 0]# The world! 
+ 
 
     # The function for printing the world status
 function worldPrint(world::Array, shape = "3.txt")
     """
     input:
         world: 
-        | An array which consist of 1 for full cell and 0 for empty cell
+            | An array which consist of 1 for full cell and 0 for empty cell
         shape:
-        | An argument which is a file consisting of the shapes using for printing them.
+            | An argument which is a file consisting of the shapes using for printing them.
     output:
         
     """
@@ -55,15 +59,13 @@ function worldPrint(world::Array, shape = "3.txt")
     end
 end
 
-worldPrint(A, "2.txt")
-
     # The Update function
-function rule(A::Array, K::Array)
+function worldRule(A::Array, K::Array)
     """
     Input:
-        A: is the world Array.
+        | A: is the world Array.
     Output:
-        K: is the weight distribution of the neighbors
+        | K: is the weight distribution of the neighbors
     """
     G = zeros(size(A)[1], size(A)[2])
 
@@ -73,7 +75,6 @@ function rule(A::Array, K::Array)
             S = sum(A[i-1:i+1, j-1:j+1] .* K)
 
             Gij = Dict(0 => -1, 1 => -1, 2 => 0, 3 => 1, 4 => -1, 5 => -1, 6 => -1, 7 => -1, 8 => -1) # The Dictionary for G value (Like a discrete function) 
-            
             G[i, j] = Gij[S]
         end
     end
@@ -81,31 +82,55 @@ function rule(A::Array, K::Array)
     return [floor(Int64, x) for x in G]
 end
 
-G = rule(A, K)
-
-ANext = A + G
-
-for i in 1:size(ANext)[1]
-    for j in 1:size(ANext)[2]
-        if ANext[i, j] == -1
-            ANext[i, j] = 0
-        elseif ANext[i, j] == 2
-            ANext[i, j] = 1
+    # The correction function
+function worldCorrection(A::Array)
+    """
+    Input:
+        | The World which consist of numbers other than 0 or 1
+    Output:
+        | Set the numbers bigger than 1 => 1, and Set the number less than 0 => 0
+    * This set up is just for descrete Game of Life cellular Automaton
+    """
+    for i in 1:size(A)[1]
+        for j in 1:size(A)[2]
+            if A[i, j] < 0
+                A[i, j] = 0
+            elseif A[i, j] > 1
+                A[i, j] = 1
+            end
         end
+    end
+    return A
+end
+
+
+    # A function for evolving the world
+function worldSimulation(A0::Array, K::Array, Nt::Int64, mode::String)
+    """
+    Input:
+        | A0  : The Initial world (configuration)
+        | K   : The Neighbor's distribution
+        | Nt  : The number of time-steps
+    Output:
+        | The worldPrint, showing the evolution of configurations 
+    """
+        # Printing the initial world
+    worldPrint(A0, mode)
+        # Initialization
+    G = worldRule(A0, K)
+    Ai = worldCorrection(A0 + G)
+    worldPrint(Ai, mode)
+    
+        # Updating
+    for i in 2:Nt
+        G   = worldRule(Ai, K)
+        Ai  = worldCorrection(Ai + G)
+        worldPrint(Ai, mode)
     end
 end
 
 
-worldPrint(ANext, "2.txt")
-
-
-
-
-
-
-
-
-
+worldSimulation(A0, K, Nt, "2.txt")
 
 
 
